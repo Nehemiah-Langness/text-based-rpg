4d92caf93d6bf078bc8e51cc81afeec17fc48439
Nehemiah Langness <Nehemiah.Langness@hotmail.com>
2026-04-11 19:42:42 -0500

    fixed build error in debug file

--- src/game/rooms/debug.ts
+++ src/game/rooms/debug.ts
@@ -4,7 +4,7 @@ import { DialogueTree } from '../engine/dialogue-tree';
 import { Quests } from '../quests';
 import { DeepCoralReef } from './open-ocean/deep-coral-reef';
 //import { Quests } from '../quests';
-import { KelpForest } from './open-ocean/kelp-forest';
+// import { KelpForest } from './open-ocean/kelp-forest';
 // import { Shipwreck } from './open-ocean/shipwreck';
 
 export default () => {
